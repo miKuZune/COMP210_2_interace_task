42ffaeb61ed42bc708e7c0640344294531fe1cff
mikuzune <maxwfarley@live.co.uk>
2017-12-14 00:44:25 +0000

    Lighting changes. New scripts to handle the laser beam by tapping on the screen and being able to use mic after a certain amount of time not using the screen.

---
+++ AR cyclops sim/Assets/Maxs/Scripts/LaserModeChooser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserModeChooser : MonoBehaviour {
+
+    public GameObject player;
+    MicButtonInput MBI;
+    MicHandler MH;
+
+
+    float timeSinceTouchPress;
+    float timeToModeChange;
+    // Use this for initialization
+    void Start () {
+        MBI = player.GetComponent<MicButtonInput>();
+        MH = player.GetComponent<MicHandler>();
+
+        timeToModeChange = 2.25f;
+
+        MH.enabled = false;
+        MBI.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceTouchPress += Time.deltaTime;
+
+        if(timeSinceTouchPress > timeToModeChange && Input.touchCount <= 0)
+        {
+            MBI.enabled = false;
+            MH.enabled = true;
+        }else if(Input.touchCount > 0)
+        {
+            MBI.enabled = true;
+            MH.enabled = false;
+            timeSinceTouchPress = 0;
+        }
+
+    }
+}
---
+++ AR cyclops sim/Assets/Maxs/Scripts/MicButtonInput.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicButtonInput : MonoBehaviour {
+
+    public GameObject laser;
+    public AudioClip[] laserSounds;
+
+    AudioSource laserAS;
+
+    float tempPressure;
+
+    float timer;
+    float maxTimer;
+    float soundTimer;
+    // Use this for initialization
+    void Start () {
+        tempPressure = 0;
+        laser.SetActive(false);
+        maxTimer = 1.25f;
+
+        laserAS = laser.GetComponent<AudioSource>();
+	}
+
+    // Update is called once per frame
+    void Update () {
+        
+        //For testing in engine.
+        /*if (Input.GetKeyDown(KeyCode.Mouse0) && laser.activeInHierarchy == false && timer > maxTimer)
+        {
+            laser.SetActive(true);
+            timer = 0;
+        }else if(timer > maxTimer && laser.activeInHierarchy == true)
+        {
+            laser.SetActive(false);
+            timer = 0;
+        }*/
+
+
+        //OnMobile
+        if (Input.touchCount > 0 && laser.activeInHierarchy == false && timer > maxTimer)
+        {
+            laser.SetActive(true);
+            timer = 0;
+            maxTimer = maxTimer / 2;
+        }
+        else if (timer > maxTimer && laser.activeInHierarchy == true)
+        {
+            laser.SetActive(false);
+            timer = 0;
+            maxTimer = maxTimer * 2;
+        }
+
+
+        //Sound Handling
+        if(laser.activeInHierarchy == true)
+        {
+            if(soundTimer < 0.5f)
+            {
+                laserAS.clip = laserSounds[0];
+            }else
+            {
+                laserAS.clip = laserSounds[1];
+            }
+
+            if (!laserAS.isPlaying)
+            {
+                laserAS.Play();
+            }
+
+            soundTimer += Time.deltaTime;
+        }else
+        {
+            soundTimer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
